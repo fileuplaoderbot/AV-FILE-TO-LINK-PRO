de56e07a4000acfcc81212fda141eed34cf59669
fileuplaoderbot <tparveen2728@gmail.com>
2025-10-01 00:13:44 +0530

    Update info.py
    Signed-off-by: fileuplaoderbot <tparveen2728@gmail.com>

--- info.py
+++ info.py
@@ -51,11 +51,11 @@ DB_URL = environ.get('DATABASE_URI', "")  # MongoDB connection URI
 DB_NAME = environ.get('DATABASE_NAME', "")  # MongoDB database name
 
 # 📸 all Media (Images)
-QR_CODE = environ.get('QR_CODE', 'https://graph.org/file/6afb4093d5ec5c4176979.jpg')  # QR Code image
-VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")  # Verify success image
-AUTH_PICS = environ.get('AUTH_PICS', 'https://envs.sh/AwV.jpg')  # Auth step image
-PICS = environ.get('PICS', 'https://envs.sh/_pM.jpg')  # Default info image
-FILE_PIC = environ.get('FILE_PIC', 'https://i.ibb.co/bj4My0bW/photo-2025-07-21-02-15-21-7529360175656861700.jpg') # file image 
+QR_CODE = environ.get('QR_CODE', 'https://i.supaimg.com/edf893d6-f4f9-46e6-a9f1-fb282b9816d7.jpg')  # QR Code image
+VERIFY_IMG = environ.get("VERIFY_IMG", "https://i.supaimg.com/edf893d6-f4f9-46e6-a9f1-fb282b9816d7.jpg")  # Verify success image
+AUTH_PICS = environ.get('AUTH_PICS', 'https://i.supaimg.com/edf893d6-f4f9-46e6-a9f1-fb282b9816d7.jpg')  # Auth step image
+PICS = environ.get('PICS', 'https://i.supaimg.com/edf893d6-f4f9-46e6-a9f1-fb282b9816d7.jpg')  # Default info image
+FILE_PIC = environ.get('FILE_PIC', 'https://i.supaimg.com/edf893d6-f4f9-46e6-a9f1-fb282b9816d7.jpg') # file image 
 
 # 📝 File Captions
 FILE_CAPTION = environ.get('FILE_CAPTION', f"{script.CAPTION}")  # Caption for single file
